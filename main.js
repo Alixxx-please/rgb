28b704f74294a02ad72a6b7db845df0b037c4d31
Alixxx-please <user012120@gmail.com>
2024-02-03 06:06:51 +0100

    Started to add search support + improved copy

--- main.js
+++ main.js
@@ -159,58 +159,74 @@ function randomColor() {
     let rgbColor = 'rgb(';
     for (let i = 0; i < 6; i++) {
         color += values[Math.floor(Math.random() * 16)];
-    }
+    };
     for (let i = 1; i < 6; i += 2) {
-        let decimal = parseInt(color.substr(i, 2), 16);
+        let decimal = parseInt(color.substring(i, i + 2), 16);
         rgbColor += decimal;
         if (i < 5) {
             rgbColor += ', ';
-        }
-    }
+        };
+    };
     rgbColor += ')';
     return {hex: color, rgb: rgbColor};
-}
+};
 
 let color = randomColor();
 
 window.onload = () => {
     document.body.style.backgroundColor = color.hex;
+    search.focus();
 };
 
 let main = document.querySelector('p');
 main.addEventListener('click', () => {
     if (main.innerText.startsWith('#') || main.innerText.includes('rgb')) {
+        let content = '';
         if (mode === 'hex') {
             navigator.clipboard.writeText(color.hex);
+            content = color.hex;
         } else if (mode === 'rgb') {
             navigator.clipboard.writeText(color.rgb);
+            content = color.rgb;
         };
-        const popup = document.getElementById('popup');
-        popup.classList.add('show');
-        setTimeout(() => popup.classList.remove('show'), 2000);
+        main.innerText = 'Copied color to clipboard!';
+        setTimeout(() => {
+            main.innerText = content;
+        }, 1800);
     };
 });
 
-let mode = 'hex';
+search.addEventListener('input', (e) => {
+    e.preventDefault();
+    main.innerText = search.value;
+});
 
+let mode = 'hex';
 window.addEventListener('keydown', (e) => {
     if (e.shiftKey) {
         mode = mode === 'hex' ? 'rgb' : 'hex';
         if (mode === 'hex') {
             main.innerText = color.hex;
+            search.placeholder = color.hex;
         } else {
             main.innerText = color.rgb;
+            search.placeholder = color.rgb;
         };
     };
     if (e.key === ' ') {
+        e.preventDefault();
         color = randomColor();
         if (mode === 'hex') {
             main.innerText = color.hex;
+            search.placeholder = color.hex;
         } else {
             main.innerText = color.rgb;
+            search.placeholder = color.rgb;
         };
         document.body.style.backgroundColor = color.hex;
         main.style.userSelect = 'all';
         main.style.cursor = 'text';
+        search.focus();
+        search.value = '';
     };
 });
